3b53fc747e87e143ef883abab77c1d427ea0275f
Luis <luis.maiworm@mhp.com>
2024-06-16 10:15:22 +0200

    gran camino

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import './App.css'
 import { DayCountdown } from './components/Timer'
 
 
+const GranCamino = () => {
+  return(
+    <h1>
+      Gran Camino! Berlin Edition
+    </h1>
+  )
+}
+
 function App() {
 
   return (
     <>
       <div className='page-wrapper'>
+        <GranCamino/>
         <DayCountdown/>
       </div>
     </>
